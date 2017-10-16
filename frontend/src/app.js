7c22ffed1a5751a06abfee6e100979629a510d52
Rodrigo Wippel <rodrigow1992@gmail.com>
2017-10-16 21:38:22 -0200

    feat: routing structure

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,16 +1,22 @@
 'use strict'
 
 import React, { PureComponent } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+import Home from './containers/home'
 
 import 'css/main.styl'
-
 import style from 'css/app.styl'
 
 class App extends PureComponent {
   render () {
     return (
       <div className={style.container}>
-        <h1>Hi udacity project!</h1>
+        <Router>
+          <Switch>
+            <Route path='/' component={Home} />
+          </Switch>
+        </Router>
       </div>
     )
   }
