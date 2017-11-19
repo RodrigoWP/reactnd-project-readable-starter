6a303fa6d7da53f6141b74d299fbaf77bb4d62e5
Rodrigo Wippel <rodrigow1992@gmail.com>
2017-11-19 10:13:06 -0200

    refactor: tranformando home em um componente função

--- frontend/src/containers/home/index.js
+++ frontend/src/containers/home/index.js
@@ -1,23 +1,20 @@
 'use strict'
 
-import React, { PureComponent } from 'react'
+import React from 'react'
 import Categories from './categories'
 import Posts from './posts'
 
 import style from './home.styl'
 
-class Home extends PureComponent {
-  render () {
-    const { match } = this.props
-    const { category } = match.params
+const Home = ({ match }) => {
+  const { category } = match.params
 
-    return (
-      <div className={style.container}>
-        <Categories key='categories' categorySelected={category} />
-        <Posts key='posts' categorySelected={category} />
-      </div>
-    )
-  }
+  return (
+    <div className={style.container}>
+      <Categories key='categories' categorySelected={category} />
+      <Posts key='posts' categorySelected={category} />
+    </div>
+  )
 }
 
 export default Home
